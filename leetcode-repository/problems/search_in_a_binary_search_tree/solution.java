da474befd91635004f5d303c9142e7289721a085
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-04-17 11:52:27 +0000

    Sync LeetCode submission - Search in a Binary Search Tree (java)

--- leetcode-repository/problems/search_in_a_binary_search_tree/solution.java
+++ leetcode-repository/problems/search_in_a_binary_search_tree/solution.java
@@ -15,13 +15,14 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null || root.val == val){
-            return root;
+        while(root!=null){
+            if(root.val<val){
+                root=root.right;
+            }
+             else if(root.val>val){
+                root=root.left;
+            } else return root;
         }
-        if(val>root.val)
-            return searchBST(root.right, val);
-        else
-            return searchBST(root.left, val);
-        
+        return null;
     }
 }
