16f27ab792d9a725ae819be1e1f501c52da32a21
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-04-29 06:00:27 +0000

    Sync LeetCode submission - Binary Search (java)

--- leetcode-repository/problems/binary_search/solution.java
+++ leetcode-repository/problems/binary_search/solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(target<nums[0])
-            return -1;
-        int low=0;
-        int high=nums.length-1;
+        int low=0, high=nums.length-1;
         while(low<=high){
-            int mid= low + (high-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             } else if(nums[mid]>target){
@@ -15,5 +12,5 @@ class Solution {
             }
         }
         return -1;
-  }
+    }
 }
