6472c63bcbecdbb706aa4b6ee280f7cbeb19d70c
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-05-24 06:32:50 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- leetcode-repository/problems/climbing_stairs/solution.java
+++ leetcode-repository/problems/climbing_stairs/solution.java
@@ -1,22 +1,14 @@
 class Solution {
-    public int climbStairs(int n) {
-        if(n==1)
-            return 1;
-        
-        int[] f = new int[n+1];
-        f[0] = 1;
-        f[1] = 1;
-        return calculate(n, 1, f);
-    }
     
-    //f[n] = f[n-1]+f[n-2];
-    public static int calculate(int n, int sum, int[] f){
-        
-        if(sum == n){
-            return f[sum];
+    int[] steps = new int[46];
+    public int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
         }
-        sum++;
-        f[sum] = f[sum-1] + f[sum-2]; 
-        return calculate(n, sum, f);
+        if(steps[n]!=0){
+            return steps[n];
+        }
+        
+        return steps[n]= climbStairs(n-1) + climbStairs(n-2);
     }
 }
