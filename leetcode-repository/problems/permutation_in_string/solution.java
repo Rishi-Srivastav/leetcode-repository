b385b808f4d1dcc560b34e97c72d82fb6a729609
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-06-01 14:05:16 +0000

    Sync LeetCode submission - Permutation in String (java)

--- leetcode-repository/problems/permutation_in_string/solution.java
+++ leetcode-repository/problems/permutation_in_string/solution.java
@@ -1,35 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] table=new int[26];
-        int m=s1.length();
-        int n= s2.length();
-        
-        if(m>n){
-          return false;   
+        int l1=s1.length();
+        int l2=s2.length();
+        char[] az=new char[26];
+        for(int i=0;i<l1;i++){
+            az[s1.charAt(i)-'a']++;
         }
-        
-        for(int i=0;i<m;i++){
-            table[s1.charAt(i)-'a']++;
-            table[s2.charAt(i)-'a']--;
-        }
-       
-        if(zeroCount(table))
-            return true;
-            
-        for(int i=m;i<n;i++){
-            table[s2.charAt(i)-'a']--;
-            table[s2.charAt(i-m)-'a']++;
-            if(zeroCount(table))
+       int start=0, end=0;
+        for(int i=0;i<=l2-l1;i++){
+            start=i;
+            end=i+l1-1;
+            if(checkAllPresent(s2,start,end,Arrays.copyOf(az, 26))){
                 return true;
-        }
+            }
+      }
         return false;
     }
     
-    
-    public boolean zeroCount(int[] t){
-        for(int i=0;i<t.length;i++){
-           if(t[i]!=0)
-               return false;
+    public boolean checkAllPresent(String s, int start, int end, char[] c){
+        for(int i=start;i<=end;i++){
+            if(c[s.charAt(i)-'a']<=0){
+                return false;
+            }
+            c[s.charAt(i)-'a']--;
+        }
+        
+        for(int i=0;i<c.length;i++){
+            if(c[i]!=0){
+                return false;
+            }
         }
         return true;
     }
