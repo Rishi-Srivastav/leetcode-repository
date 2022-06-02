46e4b9d3880259ddd9b7ee8c682414cfad52f254
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-06-02 20:52:20 +0000

    Sync LeetCode submission - Ugly Number II (java)

---
+++ leetcode-repository/problems/ugly_number_ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        if(n==1)
+            return 1;
+        int[] dp=new int[n];
+        int i2=0,i3=0,i5=0;
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            dp[i]=Math.min(Math.min(dp[i2]*2, dp[i3]*3), dp[i5]*5);
+            if(dp[i]==dp[i2]*2)
+                i2++;
+            if(dp[i]==dp[i3]*3)
+                i3++;
+            if(dp[i]==dp[i5]*5)
+                i5++;
+        }
+        return dp[n-1];
+    }
+}
