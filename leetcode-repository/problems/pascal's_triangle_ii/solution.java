e21223fed21c03aea178e21f58742efb0602520c
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-06-07 10:43:13 +0000

    Sync LeetCode submission - Pascal's Triangle II (java)

--- leetcode-repository/problems/pascal's_triangle_ii/solution.java
+++ leetcode-repository/problems/pascal's_triangle_ii/solution.java
@@ -1,42 +1,22 @@
 class Solution {
-    
-    
-    public static List<Integer> getRow(int rowIndex) {
-       List<List<Integer>> f = new ArrayList(10);
-        List<Integer> f0 = new ArrayList(1);
-        f0.add(1);
-        f.add(f0);
-        
-        List<Integer> f1 = new ArrayList(2);
-        f1.add(1);
-        f1.add(1);
-        f.add(f1);
-        
-        //(i,j)=f(i−1,j−1)+f(i−1,j)
-        
-        if(rowIndex == 0 || rowIndex == 1){
-            return f.get(rowIndex);
-        }
-        
-        return getRowList(rowIndex, f, 1);    
-        
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        if(rowIndex==0)
+            return list;
+        return recursiveRow(list, rowIndex, 0);
     }
-      
-    public static List<Integer> getRowList(int rowIndex, List<List<Integer>> f, int start){
-            if(start == rowIndex){
-                return f.get(rowIndex);
-            }
-        start++;
-        List<Integer> f1 = new ArrayList(start+1);
     
-        for(int i=0;i<=start; i++){
-            if(i== 0 || i== start){
-                f1.add(1);
-            } else{
-                f1.add( f.get(start-1).get(i-1) + f.get(start-1).get(i));
-            }
-        }
-        f.add(f1);
-        return getRowList(rowIndex, f, start);
-        }    
+    public List<Integer> recursiveRow(List<Integer> list, int rowIndex, int n){
+        if(rowIndex==n)
+            return list;
+        
+        List<Integer> lj = new ArrayList();
+        lj.add(1);
+         for(int i=1;i<list.size();i++){
+             lj.add(list.get(i-1)+ list.get(i));
+                }
+       lj.add(1);
+       return recursiveRow(lj, rowIndex, n+1);
     }
+}
