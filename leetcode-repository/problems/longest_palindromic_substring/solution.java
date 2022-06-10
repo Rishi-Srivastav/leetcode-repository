f6dbbee93a90a1b95a0977b50f6cf8720ada37d2
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-06-10 13:16:15 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- leetcode-repository/problems/longest_palindromic_substring/solution.java
+++ leetcode-repository/problems/longest_palindromic_substring/solution.java
@@ -1,34 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int max_length =1, start=0;
-        char[] arr = s.toCharArray();
-        if(arr.length<=1)
-            return s;
-        for(int i=1;i<arr.length;i++){
-            //even-palindrome
-            int low = i-1;
-            int high = i;
-            
-            while(low>=0 && high<arr.length && arr[low]==arr[high]){
-                if(max_length<high-low+1){
-                    max_length=high-low+1;
-                    start=low;
-                }
-                low--;
-                high++;   
+        char[] c=s.toCharArray();
+        int start=0, end=0, max=1;
+        for(int i=1;i<c.length;i++){
+            //even check
+            int a=i-1, b=i;
+            while(a>=0 && b<c.length && c[a]==c[b]){
+                a--;
+                b++;
             }
-            
-            low=i-1;
-            high=i+1;
-            while(low>=0 && high<arr.length && arr[low]==arr[high]){
-               if(max_length<high-low+1){
-                    max_length=high-low+1;
-                    start=low;
-                }
-                low--;
-                high++;   
+            if(b-a+1>max){
+                start=a+1;
+                end=b-1;
+                max=b-a+1;
             }
+            System.out.println("max="+max+ ", start="+start+", end="+end);
+            a=i-1;
+            b=i+1;
+            //odd check
+            while(a>=0 && b<c.length && c[a]==c[b]){
+                a--;
+                b++;
             }
-           return s.substring(start, start+max_length);
+            if(b-a+1>max){
+                start=a+1;
+                end=b-1;
+                max=b-a+1;
+            }
+            System.out.println("max="+max+ ", start="+start+", end="+end);
         }
+        
+        return s.substring(start,end+1);
     }
+}
