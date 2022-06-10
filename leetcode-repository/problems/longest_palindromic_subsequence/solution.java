e3052101b37129fe8b90b30de698dd3b5cc9e4ec
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-06-10 19:49:59 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (java)

---
+++ leetcode-repository/problems/longest_palindromic_subsequence/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestPalindromeSubseq(String ss) 
+    {
+        char[] s = ss.toCharArray();
+        
+        int n = s.length;
+        int[][] dp = new int[n][n];
+        
+        // len 1
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
+        
+        // len 2
+        for (int i = 0, j = 1; j < n; i++, j++) dp[i][j] = s[i] == s[j] ? 2 : 1;
+        
+        // len 3 and up
+        for (int len = 3; len <= n; len++)
+        {
+            for (int i = 0, j = len - 1; j < n; i++, j++)
+            {
+                // better of without left or without right
+                int max = Math.max(dp[i][j-1], dp[i+1][j]);
+                if (s[i] == s[j])
+                {
+                    // now check 2 plus without left and without right
+                    max = Math.max(max, 2 + dp[i+1][j-1]);
+                }
+                dp[i][j] = max;
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+}
