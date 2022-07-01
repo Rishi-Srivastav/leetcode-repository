d6baad4ff611427f55d415d32e303fac822d3348
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-07-01 13:11:17 +0530

    added recursive solution

--- leetcode-repository/problems/coin_change/solution.java
+++ leetcode-repository/problems/coin_change/solution.java
@@ -15,5 +15,34 @@ class Solution {
        } 
        return dp[amount] > amount ? -1 : dp[amount];
     }
+    
+      public int coinChangeRecursive(int[] coins, int amount) {
+       Arrays.sort(coins);
+       return coinMin(coins, amount, coins.length-1, 0); 
+    }
+    
+    public int coinMin(int[] coins, int amount, int i, int count){
+        if(amount==0){
+            return count;
+        }
+        if(amount<0 || i<0){
+            return -1;
+        }
+        
+       int notadded = i-1>=0 ? coinMin(coins, amount, i-1, count) :-1;
+       int added =  amount-coins[i]>=0 ? coinMin(coins, amount-coins[i], i, count+1) : -1;
+       
+       if(notadded==-1){
+           count= added;
+       } else if(added==-1){
+           count= notadded;
+       } else if(added!=-1 && notadded!=-1){
+           count= Math.min(added, notadded);
+       } else {
+           count= -1;
+       }
+        return count;
+    }
+}
 
 }
