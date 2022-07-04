e7bbec16b6109a65922061b48cb626220f016086
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-07-04 21:04:16 +0000

    Sync LeetCode submission - Permutations (java)

--- leetcode-repository/problems/permutations/solution.java
+++ leetcode-repository/problems/permutations/solution.java
@@ -1,38 +1,36 @@
 class Solution {
-   public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> lists = new ArrayList<>();
-        gen(nums, nums.length, lists);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> lists=new ArrayList();
+        permute(lists, nums, 0);
         return lists;
+        
     }
-
-    public  static void gen(int[] input, int k, List<List<Integer>> lists){
-        if(k==1){
-            List<Integer> out = new ArrayList();
-            for(int i=0;i<input.length;i++){
-                out.add(input[i]);
+    
+    public void permute(List<List<Integer>> lists, int[] nums, int start){
+        if(start==nums.length-1){
+            List<Integer> list=new ArrayList();
+            for(int k:nums) {
+                list.add(k);
             }
-            lists.add(out);
+            lists.add(list);
             return;
         }
-
-        // Generate permutations with kth unaltered
-        // Initially k == length(A)
-        gen(input, k-1, lists);
-
-        // Generate permutations for kth swapped with each k-1 initial
-        for(int i=0;i<k-1;i++){
-            if(k%2==0)
-                swap(input, i, k-1);
-            else
-                swap(input, 0, k-1);
-
-            gen(input, k-1, lists);
+        
+        
+        for(int i=start;i<nums.length;i++){
+            swap(nums, i, start);
+            permute(lists, nums, start+1);
+            swap(nums,start,i);
         }
     }
-
-    public static void swap(int[] input, int i, int j){
-        int tmp = input[i];
-        input[i]= input[j];
-        input[j]=tmp;
+    
+    public int[] swap(int[] arr , int i, int j){
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+        return arr;
     }
+    
+
+    
 }
