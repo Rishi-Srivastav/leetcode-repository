417a98e83a9910e81d1d9d112be4b4f0b980aeae
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2022-07-11 17:47:57 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- leetcode-repository/problems/binary_tree_level_order_traversal/solution.java
+++ leetcode-repository/problems/binary_tree_level_order_traversal/solution.java
@@ -14,30 +14,32 @@
  * }
  */
 class Solution {
-
-       public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> lists = new ArrayList<>();
-        if(root==null)
-            return lists;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (true) {
-            List<Integer> list = new ArrayList<>();
-            List<TreeNode> child=new ArrayList<>();
-            while (!q.isEmpty()) {
-                TreeNode node=q.poll();
-                if(node!=null)
-                    list.add(node.val);
-                if(node.left!=null)
-                    child.add(node.left);
-                if(node.right!=null)
-                child.add(node.right);
-            }
-            lists.add(list);
-            q.addAll(child);
-            if(child.isEmpty()){
-                return lists;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists= new ArrayList<>();
+        
+        if(root==null) return lists;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        List<Integer> list=new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode n=queue.remove();
+            if(n==null){
+                lists.add(list);
+                list=new ArrayList();
+                if(!queue.isEmpty())
+                    queue.add(null);
+            } else {
+                list.add(n.val);
+                if(n.left!=null){
+                    queue.add(n.left);
+                }
+                if(n.right!=null){
+                    queue.add(n.right);
+                }
             }
         }
-       }
+        return lists;
+    }
 }
